e5749452901c3069ab70dfc8510885b059c6b147
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2023-08-30 21:58:51 +0800

    - Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,85 +1,75 @@
+import Link from 'next/link'
+import { Fragment } from 'react'
+import { Menu } from '@headlessui/react'
+
+const links = [
+	{ href: '/', label: 'Find Problems' },
+	{ href: '/upload', label: 'Upload Problems' },
+	{ href: '/Signin', label: 'Signin' },
+	{ href: '/SignOut', label: 'Signout' },
+]
+
 function Navbar() {
 	return (
 		// a tailwind navbar
-		<nav className="bg-phDarkgrey border-gray-200">
-			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-				<a href="https://problemhub.com/" className="flex items-center">
-					<img
-						src="https://flowbite.com/docs/images/logo.svg"
-						className="h-8 mr-3"
-						alt="Flowbite Logo"
-					/>
-					<span className="self-center text-2xl font-semibold whitespace-nowrap text-phLinen">
+		<nav className="border-gray-200 bg-phDarkgrey font-manjari text-lg">
+			<div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+				<Link href="/" className="flex items-center">
+					<span className="self-center whitespace-nowrap font-monoton text-2xl text-phLinen md:text-5xl">
 						ProblemHub
 					</span>
-				</a>
-				<button
-					data-collapse-toggle="navbar-default"
-					type="button"
-					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-					aria-controls="navbar-default"
-					aria-expanded="false"
-				>
-					<span className="sr-only">Open main menu</span>
-					<svg
-						className="w-5 h-5"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 17 14"
-					>
-						<path
-							stroke="currentColor"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M1 1h15M1 7h15M1 13h15"
-						/>
-					</svg>
-				</button>
-				<div className="hidden w-full md:block md:w-auto" id="navbar-default">
-					<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-						<li>
-							<a
-								href="#"
-								className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
-								aria-current="page"
-							>
-								Home
-							</a>
-						</li>
-						<li>
-							<a
-								href="#"
-								className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-							>
-								About
-							</a>
-						</li>
-						<li>
-							<a
-								href="#"
-								className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-							>
-								Services
-							</a>
-						</li>
-						<li>
-							<a
-								href="#"
-								className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-							>
-								Pricing
-							</a>
-						</li>
-						<li>
-							<a
-								href="#"
-								className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-							>
-								Contact
-							</a>
-						</li>
+				</Link>
+				<Menu>
+					<Menu.Button className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 lg:hidden">
+						<span className="sr-only">Open main menu</span>
+						<svg
+							className="h-5 w-5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 17 14"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M1 1h15M1 7h15M1 13h15"
+							/>
+						</svg>
+					</Menu.Button>
+					<Menu.Items className="absolute right-0 top-24 mr-4 flex flex-col rounded-md bg-phDarkgrey p-4">
+						{links.map((link) => (
+							/* Use the `active` state to conditionally style the active item. */
+							<Menu.Item key={link.href} as={Fragment}>
+								{({ active }) => (
+									<a
+										href={link.href}
+										className={`${
+											active
+												? 'bg-phDarkgrey text-phLinen'
+												: 'bg-phDarkgrey text-white'
+										}`}
+									>
+										{link.label}
+									</a>
+								)}
+							</Menu.Item>
+						))}
+					</Menu.Items>
+				</Menu>
+				<div className="hidden w-full lg:block lg:w-auto" id="navbar-default">
+					<ul className="mt-4 flex flex-col rounded-lg border border-gray-100 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-phDarkgrey md:p-0">
+						{links.map((link) => (
+							<li key={link.href}>
+								<a
+									href={link.href}
+									className="block rounded bg-phDarkgrey py-2 pl-3 pr-4 text-white md:p-0 hover:text-black"
+								>
+									{link.label}
+								</a>
+							</li>
+						))}
 					</ul>
 				</div>
 			</div>
