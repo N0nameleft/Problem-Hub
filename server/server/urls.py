9f42a895e2b212e367b0b1968319220aae5ea488
Jazmyn <23067296@student.uwa.edu.au>
2023-09-09 17:26:09 +0800

    added amdmin url

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from problems import views
@@ -7,6 +8,7 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Adding the admin site URLs
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
