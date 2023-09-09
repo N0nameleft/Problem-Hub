500215531c3dfc3e8f699e078f4669829f4c3acf
Jazmyn <23067296@student.uwa.edu.au>
2023-09-09 22:01:37 +0800

    added a views and serializers file

---
+++ server/server/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import User
+from .models import Fact
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('user_id', 'name', 'username', 'email', 'password')
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fact
+        fields = ('problem_id', 'problem_name', 'problem_data', 'date_added', 'link_to_file', 'user_id')
