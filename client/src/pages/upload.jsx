1c9548450af6e6562bd4d11f3fbf737ef16bc73d
Stefan Nio <stefnio01@gmail.com>
2023-09-21 17:21:14 +0800

    added functionality to pop up required zip file format (not actual instructions)

--- client/src/pages/upload.jsx
+++ client/src/pages/upload.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState }from 'react'
 import Navbar from '../components/Navbar'
 
 const Upload = () => {
-    const [selectedFile, setSelectedFile] = React.useState('');
+    const [selectedFile, setSelectedFile] = useState('');
+    const [selectedOption, setSelectedOption] = useState('single');
+    const [showProblemFormat, setShowProblemFormat] = useState(false);
 
 	const handleFileChange = (e) => {
 		const selectedFile = e.target.files[0]
@@ -14,6 +16,14 @@ const Upload = () => {
 		}
 	}
 
+    const handleOptionChange = (e) => {
+        setSelectedOption(e.target.value);
+    }
+
+    const toggleProblemFormat = () => {
+        setShowProblemFormat(!showProblemFormat);
+    }
+
 	return (
 		<>
 			<Navbar />
@@ -24,24 +34,59 @@ const Upload = () => {
 					</div>
 					{/*make a button underneath that says "Select File" and when you click it, it takes you to a file explorer and you can select a file to upload*/}
 					<div className="flex flex-col items-center rounded-b-lg bg-phDarkergrey p-4 text-white">
+						<div className="mb-4 flex justify-center">
+							<label className="mr-4">
+								<input
+									type="radio"
+									value="single"
+									checked={selectedOption === 'single'}
+									onChange={handleOptionChange}
+								/>
+								Select zip file containing 1 Problem set
+							</label>
+							<label>
+								<input
+									type="radio"
+									value="multiple"
+									checked={selectedOption === 'multiple'}
+									onChange={handleOptionChange}
+								/>
+								Select zip file containing multiple Problem sets
+							</label>
+						</div>
 						<input
 							type="file"
-							accept=".zip" // Specify the file type you want to accept (in this case, .zip)
-							className="hidden" // Hide the input visually
-							id="fileInput" // Add an ID to the input element
-							onChange={handleFileChange} // Call a function when the file selection changes
+							accept=".zip"
+							className="hidden"
+							id="fileInput"
+							onChange={handleFileChange}
 						/>
 						<label
 							htmlFor="fileInput"
 							className="cursor-pointer rounded-lg bg-phDarkgrey px-4 py-2 text-phLinen hover:bg-gray-600"
 						>
-							Select File
+							{selectedOption === 'single'
+								? 'Select File (1 Problem set)'
+								: 'Select File (Multiple Problem sets)'}
 						</label>
 						<p className="mt-4 text-center">{selectedFile}</p>
 						<p className="mt-4 text-center">
 							Note: Please follow the problem-tools format for structuring the
-							problems (must be directly inside a zip file).
+							problems (must be directly inside a zip file).{' '}
+							<button
+								className="ml-2 cursor-pointer text-phLinen hover:text-phGreen underline"
+								onClick={toggleProblemFormat}
+							>
+								Click here to see the correct Problem Tools Format!
+							</button>
 						</p>
+						{showProblemFormat && (
+							<div className="mt-4">
+								<p>Here is the correct Problem Tools Format:</p>
+								{/* Add your detailed format explanation here */}
+								<p className='italic'>This is where we explain the format in detail.</p>
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
