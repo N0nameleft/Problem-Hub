87722e9e531926736ba17bf23d883392f15873e0
Jazmyn <23067296@student.uwa.edu.au>
2023-09-28 16:28:13 +0800

    troubleshooting and new API

--- server/server/serializers.py
+++ server/server/serializers.py
@@ -9,10 +9,11 @@ class UserSerializer(serializers.ModelSerializer):
 
 class ProblemSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source='user.username', read_only=True)  # Get username from the related User model
-    problem_desc = serializers.CharField(source='problem_name', read_only=True)  # Rename problem_name to problem_desc
-    creation_date = serializers.DateField(source='date_added', read_only=True)  # Rename date_added to creation_date
+    #problem_name = serializers.CharField(source='problem_name', read_only=True)  # Rename problem_name to problem_desc
+    #creation_date = serializers.DateField(source='date_added', read_only=True)  # Rename date_added to creation_date
+   # problem_data = serializers.models.CharField(source="problem_data", read_only = True)
+
     class Meta:
         model = Problem
-        # fields = ('problem_id', 'problem_name', 'problem_data', 'date_added', 'link_to_file', 'user_id')
-        fields = '__all__'
+        fields = ("user_name", 'problem_name',"problem_data","date_added")
 
