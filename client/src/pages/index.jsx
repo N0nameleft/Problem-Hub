b9ec6495d8e2266193711f5a988452011a2ae1bc
Stefan Nio <stefnio01@gmail.com>
2023-10-08 00:46:45 +0800

    add feature checkbox, add feature download button, add feature truncate tailwind

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -6,15 +6,52 @@ import Latex from 'react-latex-next'
 function HomePage({ problems }) {
 	const [viewingProblem, setViewingProblem] = useState(null)
 
+	// State for Selected Problems
+	const [selectedProblems, setSelectedProblems] = useState([]) // Array of problem IDs
+
 	const handleProblemClick = (problem) => {
 		setViewingProblem(problem)
 	}
+
+	// Function to handle checkbox change
+	const handleCheckboxChange = (problemId) => {
+		// check if problemId is in selectedProblems array
+		if (selectedProblems.includes(problemId)) {
+			// remove problemId from selectedProblems array
+			setSelectedProblems(selectedProblems.filter((id) => id !== problemId))
+		} else {
+			// add problemId to selectedProblems array
+			setSelectedProblems([...selectedProblems, problemId])
+		}
+
+		// implement logic with selectedProblems array here
+		// for now just console.log it
+		console.log(selectedProblems)
+	}
+
+	// Function to handle download
+	const handleDownload = () => {
+		// Implement the download logic here
+		console.log('Downloading selected problems:', selectedProblems)
+	}
+
 	return (
 		<>
 			<Navbar />
 			<div className="h-screen bg-phDarkgrey px-48 pt-24">
 				<div className="no-scrollbar absolute bottom-0 left-0 right-0 top-0 mx-48 mt-24 h-auto overflow-scroll">
-					<h2 className="text-white">Find contest problems</h2>
+					<div className="mb-2 flex items-center justify-between">
+						<h2 className="text-white">Find contest problems</h2>
+						{/* Download button */}
+						{selectedProblems.length > 0 && (
+							<button
+								onClick={handleDownload}
+								className="rounded-lg bg-phDarkergrey px-4 py-2 text-white shadow-md hover:bg-gray-600"
+							>
+								Download Selected Problems
+							</button>
+						)}
+					</div>
 					<div className=" mt-2 h-full overflow-hidden border-4 border-phDarkergrey bg-phGreen">
 						{/* A table with the rows "problem", "Uploaded by" and "Uploaded on" */}
 						{viewingProblem ? (
@@ -47,21 +84,29 @@ function HomePage({ problems }) {
 											key={problem.id}
 											className="h-9 bg-phDarkgrey align-bottom hover:bg-phDarkergrey"
 										>
-											<td colSpan={2} className="pl-48 text-left">
-												<a
-													onClick={() =>
-														handleProblemClick({
-															id: problem.id,
-															Problem_Name: problem.Problem_Name,
-															Uploaded_By: problem.Uploaded_By,
-															Date_Uploaded_On: problem.Date_Uploaded_On,
-															Problem_Latex: problem.Problem_Data,
-														})
-													}
-													href="#"
-												>
-													{problem.Problem_Name}
-												</a>
+											<td className="pl-48 text-left">
+												<input
+													type="checkbox"
+													onChange={() => handleCheckboxChange(problem.id)}
+												/>
+											</td>
+											<td className="text-left" style={{ maxWidth: '400px' }}>
+												<div className="truncate">
+													<a
+														onClick={() =>
+															handleProblemClick({
+																id: problem.id,
+																Problem_Name: problem.Problem_Name,
+																Uploaded_By: problem.Uploaded_By,
+																Date_Uploaded_On: problem.Date_Uploaded_On,
+																Problem_Latex: problem.Problem_Data,
+															})
+														}
+														href="#"
+													>
+														{problem.Problem_Name}
+													</a>
+												</div>
 											</td>
 											<td>{problem.Date_Uploaded_On}</td>
 											<td>{problem.Uploaded_By}</td>
