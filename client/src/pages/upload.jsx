ed764d3281d1321bc9339ce06a71c384181f181d
Stefan Nio <stefnio01@gmail.com>
2023-10-14 22:36:58 +0800

    extended zip format with another outer zip

--- client/src/pages/upload.jsx
+++ client/src/pages/upload.jsx
@@ -98,34 +98,38 @@ const Upload = () => {
 							<pre className="text-phLinen">
 								{`ZIP Root:
 |
-|-- problem.yaml   # Must exist at the root.
-|
-|-- data/          # Must exist.
-|   |
-|   |-- sample/           # Must exist.
-|   |   |-- .in &.ans  # Paired files with the same base name.
-|   |
-|   |-- secret/           # Must exist.
-|       |-- .in &.ans  # Paired files with the same base name.
-|
-|-- problem.pdf       # Either this,
-| OR
-|-- problem_statement/    # Or this folder must exist.
-|   |-- problem.en.tex    # Must exist if the folder exists.
-|
-|-- solutions/ OR submissions/     # At least one must exist.
-|   |
-|   |-- accepted/        # Must exist.
-|   |   |-- .cpp,.java, .py,.cc, .kt,.cs  # At least one file with these extensions.
-|   |
-|   |-- wrong_answer/    # Optional folder.
-|   |   |-- .cpp,.java, .py,.cc, .kt,.cs  # If the folder exists, at least one file with these extensions.
-|   |
-|   |-- time_limit_exceeded/  # Optional folder.
-|       |-- .cpp,.java, .py,.cc, .kt,.cs  # If the folder exists, at least one file with these extensions.
-|
-|-- output_validators/  # Optional folder.
-    |-- .cpp &.h     # If the folder exists, exactly one file with each extension.`}
+|-- ZIP Problem A (example)
+|-- ZIP Problem B (example)
+|-- ZIP Problem C (example)
+|-- ZIP Problem format
+	|-- problem.yaml   # Must exist at the root.
+	|
+	|-- data/          # Must exist.
+	|   |
+	|   |-- sample/           # Must exist.
+	|   |   |-- .in &.ans  # Paired files with the same base name.
+	|   |
+	|   |-- secret/           # Must exist.
+	|       |-- .in &.ans  # Paired files with the same base name.
+	|
+	|-- problem.pdf       # Either this,
+	| OR
+	|-- problem_statement/    # Or this folder must exist.
+	|   |-- problem.en.tex    # Must exist if the folder exists.
+	|
+	|-- solutions/ OR submissions/     # At least one must exist.
+	|   |
+	|   |-- accepted/        # Must exist.
+	|   |   |-- .cpp,.java, .py,.cc, .kt,.cs  # At least one file with these extensions.
+	|   |
+	|   |-- wrong_answer/    # Optional folder.
+	|   |   |-- .cpp,.java, .py,.cc, .kt,.cs  # If the folder exists, at least one file with these extensions.
+	|   |
+	|   |-- time_limit_exceeded/  # Optional folder.
+	|       |-- .cpp,.java, .py,.cc, .kt,.cs  # If the folder exists, at least one file with these extensions.
+	|
+	|-- output_validators/  # Optional folder.
+	    |-- .cpp &.h     # If the folder exists, exactly one file with each extension.`}
 							</pre>
 						</div>
 					)}
