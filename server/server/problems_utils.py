a6c1eefb6a0c80971e8ffefd1f2bdcf79ada8755
Zach <bookie0097@gmail.com>
2023-10-16 14:07:19 +0800

    save ini file to problem zip

--- server/server/problems_utils.py
+++ server/server/problems_utils.py
@@ -1,5 +1,5 @@
 def create_domjudge_ini(problem, allow_submit=1, allow_judge=1, timelimit=2, special_run=1, special_compare=2, points=10, color="#FF5733"):
-    ini_content = f"name = {problem.problem_name}\n"
+    ini_content = f"name = {problem}\n"
     ini_content += f"allow_submit = {allow_submit}\n"
     ini_content += f"allow_judge = {allow_judge}\n"
     ini_content += f"timelimit = {timelimit}\n"
@@ -12,11 +12,13 @@ def create_domjudge_ini(problem, allow_submit=1, allow_judge=1, timelimit=2, spe
 
 def save_domjudge_ini_to_file(problem, ini_content):
     # Determine the file path where you want to save the INI file
-    file_path = f"path/to/your/directory/{problem.problem_name}.ini"  # Adjust the path and filename as needed
-
+    file_path = f"./{problem}.ini"  # Adjust the path and filename as needed
+    file_name = f"{problem}.ini"
     # Write the INI content to the file
     with open(file_path, 'w') as ini_file:
         ini_file.write(ini_content)
+    
+    return file_path, file_name
 
 # Example usage:
 #problem = Problem.objects.get(pk=1)  # Replace 1 with the actual problem's primary key or use another method to retrieve a problem object
